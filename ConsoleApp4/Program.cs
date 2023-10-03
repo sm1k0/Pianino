72c9c264a015fa13d64a6d19864a727cf6645a98
ILYA <94749164+sm1k0@users.noreply.github.com>
2023-10-03 12:45:03 +0300

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Media;
 
 namespace Piano
@@ -7,9 +7,9 @@ namespace Piano
     {
         static int currentOctave = 4; 
         static int[][] octaves = new int[][] { 
-            new int[] {131, 147, 165, 175, 196, 220, 247}, 
-            new int[] {262, 294, 330, 349, 392, 440, 494}, 
-            new int[] {523, 587, 659, 698, 784, 880, 988} 
+            new int[] {262, 294, 330, 349, 392, 440, 494, 523}, 
+            new int[] {523, 587, 659, 698, 784, 880, 988, 1047},
+            new int[] {1047, 1175, 1319, 1397, 1568, 1760, 1976, 2093} 
         };
 
         static void Main(string[] args)
@@ -26,7 +26,7 @@ namespace Piano
                 }
                 else if (key.KeyChar == ' ') 
                 {
-                    Console.Beep(1047, 500);
+                    StopSound();
                 }
                 else 
                 {
@@ -35,7 +35,7 @@ namespace Piano
                     if (noteIndex >= 0) 
                     {
                         int frequency = octaves[currentOctave - 1][noteIndex]; 
-                        PlaySound(frequency);
+                        PlaySound(frequency); 
                     }
                 }
             }
@@ -46,17 +46,28 @@ namespace Piano
             Console.Beep(frequency, 500); 
         }
 
+        static void StopSound()
+        {
+            Console.Beep(0, 100); 
+        }
+
         static int GetNoteIndex(char key)
         {
             switch (key)
             {
                 case 'a': return 0;
-                case 's': return 1;
-                case 'd': return 2;
-                case 'f': return 3;
-                case 'g': return 4;
-                case 'h': return 5;
-                case 'j': return 6;
+                case 'w': return 1;
+                case 's': return 2;
+                case 'e': return 3;
+                case 'd': return 4;
+                case 'f': return 5;
+                case 't': return 6;
+                case 'g': return 7;
+                case 'y': return 8;
+                case 'h': return 9;
+                case 'u': return 10;
+                case 'j': return 11;
+                case 'k': return 12;
                 default: return -1; 
             }
         }
